87fae7590ec12c32bf86a3c922a4ec6884496bf3
Shod <oleg.shmyk@gmail.com>
2023-02-17 09:51:09 +0100

    setStatusProgram

---
+++ app/Http/Requests/ProgramStatusRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\BaseFormRequest;
+
+class ProgramStatusRequest extends BaseFormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'user_id'   => ['bail', 'required', 'integer'],
+            'active'    => ['boolean'],
+            'purchased' => ['boolean'],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'A name is required',
+            'description.required' => 'A description is required',
+            'description.cost' => 'A cost is required',
+        ];
+    }
+}
