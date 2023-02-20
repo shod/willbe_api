fce008fb3964837ab63f5bff3dd7e5ffc0b25b7a
Shod <oleg.shmyk@gmail.com>
2023-02-20 17:10:54 +0100

    Add mailgun mofule

---
+++ app/Console/Commands/TestMail.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ForgetEmail;
+
+class TestMail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'mail:forget';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Test mail servis MailGun';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $to_email = "oleg.shmyk@mail.com";
+
+        Mail::to($to_email)->send(new ForgetEmail);
+
+        if (!Mail::flushMacros()) {
+            return "<p> Success! Your E-mail has been sent.</p>";
+        } else {
+            return "<p> Failed! Your E-mail has not sent.</p>";
+        }
+        return Command::SUCCESS;
+    }
+}
