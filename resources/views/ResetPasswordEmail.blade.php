53a9907aad52f521bab9640909986173382456e1
Shod <oleg.shmyk@gmail.com>
2023-05-20 20:57:54 +0200

    reset_mail

---
+++ resources/views/ResetPasswordEmail.blade.php
@@ -0,0 +1,18 @@
+<div class="container" style="padding: 1rem; background: #f5f5f5;">
+  <p>Dear {{ $user_name }},</p>  
+  <p>
+    Page for change password - <a href="{{ $url }}"> Change Password </a>
+  </p>
+  <p>
+    However, where this has not been initiated by you or you suspect any unauthorized activity, please send an email to our dedicated support team at support@mywillbe.com and we will take appropriate measures to protect your account.
+  </p>
+  <p>
+    Your satisfaction and security are our top priorities, and we want to ensure that you are aware of any changes made to your account.
+  </p>
+  <p>
+    Best regards,
+  </p>
+  <p>
+    Willbe. Support Team
+  </p>
+</div>
