a3ec24f54298aea9d7cc6bf0bf40b5de3cbf31c5
Shod <oleg.shmyk@gmail.com>
2023-05-20 21:16:58 +0200

    Correct text

--- resources/views/ResetPasswordEmail.blade.php
+++ resources/views/ResetPasswordEmail.blade.php
@@ -1,7 +1,7 @@
 <div class="container" style="padding: 1rem; background: #f5f5f5;">
   <p>Dear {{ $user_name }},</p>    
   <p>
-    You have requested password reset for your account at wizzair.com. Click on the link below to get you in the door.
+    You have requested password reset for your account at MyWillbe.com. Click on the link below to get you in the door.
   </p>
   <p>
     Page for change password - <a href="{{ $url }}"> Change Password </a>
