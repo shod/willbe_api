3b8e12f8d25caa9f07063e9fe8dfc92c03a873b2
Shod <oleg.shmyk@gmail.com>
2023-05-20 20:56:00 +0200

    reset_mail

---
+++ app/Mail/ForgotEmail.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class ForgotEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user_name;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($user_name)
+    {
+        $this->user_name = "Paulo Sanchos";
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from(env('MAIL_FROM_ADDRESS'))->view('ForgotEmail');
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'Forgot Email',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'ForgotEmail'
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
